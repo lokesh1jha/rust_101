b69812702196b36b39465803296b50fda2626858
lokeshjha <lokesh1jha@gmai.com>
2024-03-13 00:32:02 +0530

    oops

---
+++ 15_oops.rs
@@ -0,0 +1,20 @@
+/*
+What Is OOP?
+Object oriented programming roughly refers to programming languages that have a number of iconic features:
+
+Encapsulation - Associating data and functions into the conceptual unit of a single type called an object.
+Abstraction - Hiding data and function members to obfuscate implementation details of an object.
+Polymorphism - The ability to interact with objects of different types through one interface.
+Inheritance - The ability to inherit data and behavior from other objects.
+
+
+## Rust Is Not OOP
+Rust lacks inheritance of data and behavior in any meaningful way.
+
+Structs cannot inherit fields from a parent struct.
+Structs cannot inherit functions from a parent struct.
+That said, Rust implements many programming language features, so that you might not mind this lacking.
+
+//https://tourofrust.com/76_en.html
+
+*/
